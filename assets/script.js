e0de6070ba80ab32a8582c89f472a2300f55400c
samelimill <samelimill@gmail.com>
2023-08-08 17:36:04 -0700

    Added comments to .js form and simplified variables via arguments

--- assets/script.js
+++ assets/script.js
@@ -7,24 +7,19 @@ let upperCaseChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O
 let numberChars = ['1','2','3','5','6','7','8','9','0'];
 let specialChars = ['!','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
 
-var password = "";
-var passwordChars = [];
-var totalChars = [];
-var passwordLength = "";
-
-// Sequence of user inputs for characters and length;
+// Sequence of functions for user inputs, password construction, and 
 function generatePassword() {
-  totalChars=[];
-  passwordLength = "";
-  totalChars = charConfirm();
-//  charCheck();
-  passwordLength = lengthPrompt(); 
-  password = passwordBuild();
+  var passwordChars = charConfirm();
+  var passwordLength = lengthPrompt();
+  var password = passwordBuild(passwordChars, passwordLength);
   return password;
 }
 
+// Gathers user preferences for password character criteria - returns array of characters
 function charConfirm() {
-  passwordChars=[];
+  // establish empty array for characters
+  var passwordChars=[];
+  // series of confirms for user to select character criteria - if confirmed, added to array
   let lowerCase = confirm("Do you want to include lowercase characters?")
   if (lowerCase==true) {passwordChars=passwordChars.concat(lowerCaseChars);}
   let upperCase = confirm("Do you want to include uppercase characters?")
@@ -33,23 +28,40 @@ function charConfirm() {
   if (numb==true) {passwordChars=passwordChars.concat(numberChars);}
   let specials = confirm("Do you want to include special characters?")
   if (specials==true) {passwordChars=passwordChars.concat(specialChars);}
+  // check to make sure at least one prompt was confirmed, otherwise reset to start of the function
+  if (passwordChars.length==0){
+    alert("Please choose at least one type of character. Try again!");
+    charConfirm();
+  }
+  // Returns user's selection of characters to generatePassword()
   return passwordChars;
 }
 
-
+// Gathers user preference for password length - returns a number
 function lengthPrompt() { 
+  // Establish empty string for password length
+  var passwordLength = "";
+  // Prompts user to input a number for password length
   let lengthCheck = prompt("How long would you like your password to be? Please enter a number between 8 and 128.")
-  if (lengthCheck < 8) {alert("Password too short, please try again.")
-  } else if (lengthCheck > 128) {alert("Password too long, please try again.")
-  } else (passwordLength=passwordLength.concat(lengthCheck));
+  // Check to make sure that password length is within 8 and 128 - if not, reset to start of function
+  if (lengthCheck < 8) {
+    alert("Password too short, please try again.");
+    lengthPrompt();
+  } else if (lengthCheck > 128) {
+    alert("Password too long, please try again.");
+    lengthPrompt();
+    // If number is within the acceptable range, return the number to generatePassword()
+  } else (passwordLength=passwordLength.concat(lengthCheck)); 
   return passwordLength;
 }
 
-function passwordBuild() {
-  password = "";
+// Construct a random password according to user criteria
+function passwordBuild(passwordChars, passwordLength) {
+  // Establish empty string for password
+  var password = "";
   for (var i = 0; i<passwordLength; i++) {
-    let randomNumb = Math.floor(Math.random()*totalChars.length);
-    let randomChar = totalChars[randomNumb];
+    let randomNumb = Math.floor(Math.random()*passwordChars.length);
+    let randomChar = passwordChars[randomNumb];
     password += randomChar;
   }
   return password;
